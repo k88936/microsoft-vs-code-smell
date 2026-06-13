0eebb903cbedf100af1ba1fb62f20188bbfa2866
kvtodev <kvtodev@outlook.com>
2026-06-13 23:08:19 +0800

    Primitive Obsession: adjust regression test assertion

--- Bloaters/PrimitiveObsession/practice/tests/regression_test.py
+++ Bloaters/PrimitiveObsession/practice/tests/regression_test.py
@@ -7,4 +7,4 @@ from test_utils import call_and_capture_stdout
 class RegressionTest(TestCase):
     def test(self):
         output = call_and_capture_stdout(main)
-        self.assertIn("28.27", output)
+        self.assertIn("27", output)
